7b3227cdb03d1c47e3436d971f628f8de47d3bba
shodav4real <shonibaredavida@yahoo.com>
2023-02-18 15:54:13 +0100

    done

--- lib/users/verified_users_screen.dart
+++ lib/users/verified_users_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:web_admin/functions/functions.dart';
 import 'package:web_admin/global/global.dart';
 import 'package:web_admin/widgets/nav_appbar.dart';
 
@@ -19,14 +20,66 @@ class _VerifiedUsersScreenState extends State<VerifiedUsersScreen> {
         .get()
         .then((verifiedUsersSnapshot) {
       if (verifiedUsersSnapshot.docs.isNotEmpty) {
-        if (dev) printo("we have data");
+        if (dev) printo("there are Verified User data");
         setState(() {
           approvedUsersSnapshot = verifiedUsersSnapshot;
         });
       } else {
-        if (dev) printo("we dont have data");
+        if (dev) printo("there is NO Verified User data");
       }
     });
+  }
+
+  void blockVerifiedUser(userID) {
+    Map<String, dynamic> userDataMap = {"status": "not approved"};
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(userID)
+        .update(userDataMap)
+        .whenComplete(() {
+      if (dev) printo('User blocked successfully');
+      showReusableSnackBar("Users has been blocked successfully!!!", context);
+    }).then((value) => Navigator.pop(context));
+  }
+
+  showDialogBox(userDocumentID) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Center(
+              child: text("Block Account",
+                  fontSize: 25,
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red),
+            ),
+            content: text("Do you want to block this account ?",
+                letterSpacing: 2,
+                fontSize: 16,
+                color: Colors.black,
+                textAlign: TextAlign.center),
+            actions: [
+              elevatedButton(
+                  onPressed: () {
+                    if (dev) printo('Admin chose NOT to delete user');
+                    Navigator.pop(context);
+                  },
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                  title: "No"),
+              elevatedButton(
+                  onPressed: () {
+                    if (dev) printo('Admin Chose to delete user');
+                    blockVerifiedUser(userDocumentID);
+                    Navigator.pop(context);
+                  },
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                  title: "Yes"),
+            ],
+          );
+        });
   }
 
   @override
@@ -43,54 +96,68 @@ class _VerifiedUsersScreenState extends State<VerifiedUsersScreen> {
           padding: const EdgeInsets.all(10),
           itemCount: approvedUsersSnapshot!.docs.length,
           itemBuilder: (context, index) {
-            if (dev) printo(approvedUsersSnapshot!.docs[index].get("photoUrl"));
-            return Card(
-              elevation: 10,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 140,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: NetworkImage(approvedUsersSnapshot!.docs[index]
-                              .get("photoUrl")),
+            return Column(
+              children: [
+                index == 0
+                    ? text(
+                        "${approvedUsersSnapshot!.docs.length} Verified Users",
+                        color: Colors.black,
+                        fontSize: 24,
+                      )
+                    : text(""),
+                Card(
+                  elevation: 10,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 140,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(approvedUsersSnapshot!
+                                  .docs[index]
+                                  .get("photoUrl")),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  text(approvedUsersSnapshot!.docs[index].get("name"),
-                      fontSize: 16, color: Colors.black),
-                  text(approvedUsersSnapshot!.docs[index].get("email"),
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                  GestureDetector(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0, top: 18),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            "images/block.png",
-                            width: 45,
+                      text(approvedUsersSnapshot!.docs[index].get("name"),
+                          fontSize: 16, color: Colors.black),
+                      text(approvedUsersSnapshot!.docs[index].get("email"),
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                      GestureDetector(
+                        onTap: () {
+                          showDialogBox(approvedUsersSnapshot!.docs[index].id);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0, top: 18),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                "images/block.png",
+                                width: 45,
+                              ),
+                              sizedBox(width: 10),
+                              text(" Block now", color: Colors.redAccent),
+                            ],
                           ),
-                          sizedBox(width: 10),
-                          text(" Block now", color: Colors.redAccent),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             );
           },
         );
       } else {
+        if (dev) printo('No Record of Verified Users');
         return Center(
             child: text("No Record of Verified Users",
                 fontSize: 30,
